a2c46aa0597249b32f44d49b7abd3ec2b2ed6add
TRpika <85185682+TRpika@users.noreply.github.com>
2021-06-07 01:34:04 +0300

    Add files via upload

---
+++ module/ui-main-carousel.php
@@ -0,0 +1,32 @@
+<div class="container fill">
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<!-- Carousel Slide -->
+			<div class="item active">
+				<div class="fill">
+					<div class='video-con-container'>
+						<div class="video-btn-play">
+						</div>
+						<div class="video-ico-service">
+						</div>
+						<div class='video-con-animate page-video-1'>
+						</div>
+						<div class='video-img-thumbnail' style="background: url('/img/videos/1.jpg') no-repeat center; background-size: cover;">
+						</div>
+						<div class="video-tx2-heading">
+						</div>
+					</div>
+					<div class='video-con-backdrop'>
+						<div class='video-wrp-backdrop'>
+						<div class="video-img-overlay darkmode-gradient">
+						</div>
+							<div class='video-img-backdrop' style="background: url('/img/videos/1b.jpg') no-repeat center bottom; background-size: cover;">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</div>
---
+++ module/ui-main-footer.php
@@ -0,0 +1,74 @@
+<footer>
+<div class="footer-con-container">
+	<div class="footer-con-overlay darkmode-header">
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+			  <div class="wavebar-svg-object"></div>
+			  <div class="wavebar-svg-object"></div>
+			</div>
+		</div>
+		<div class="footer-con-header">
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					
+					<table class="footer-table-out">
+					<tr>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<a href="https://vk.com/lesha_stepanenko" target="_blank">
+										<span>Создатль идеи</span>
+									</a>
+									</div>
+								
+					
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<a href="https://vk.com/cuttelloverius" target="_blank">
+										<span>Создатель графики</span>
+									</a>
+									</div>
+									
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<a href="https://disk.yandex.ru/d/SipiKG-uRf0i0Q" target="_blank">
+										<span>Скачать игру</span>
+									</a>
+									</div>
+									
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<a href="https://vk.com/iddpika" target="_blank">
+										<span>Создатель сайта</span>
+									</a>
+									</div>
+								
+								</div>
+							</div>
+						</td>
+						
+					</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+</div>
+</footer>
